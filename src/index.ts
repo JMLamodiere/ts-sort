437fe460d6026d33e69d3fb28b3eed70d585392e
Jean-Marie Lamodière <j.lamodiere@meetic-corp.com>
2021-09-15 18:19:33 +0200

    feat: sortable CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numberSorter = new Sorter(numbersCollection);
+numberSorter.sort();
 
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("daBiXu");
+const charactersSorter = new Sorter(charactersCollection);
+charactersSorter.sort();
+
+console.log(charactersCollection.data);
