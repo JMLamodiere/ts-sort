9570e96fd5b91ee5e5b32031130b8adc55ededae
Jean-Marie Lamodière <j.lamodiere@meetic-corp.com>
2021-09-15 18:06:31 +0200

    refactor: use interface

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ import { NumbersCollection } from "./NumbersCollection";
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection.data);
+
+console.log(numbersCollection.data);
