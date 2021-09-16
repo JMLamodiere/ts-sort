7bab5be27f289fcb80253a4f02db3cbc32b5f7c8
Jean-Marie Lamodière <j.lamodiere@meetic-corp.com>
2021-09-16 09:23:49 +0200

    feat: add sortable LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numberSorter = new Sorter(numbersCollection);
@@ -13,3 +14,13 @@ const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+
+linkedList.print();
